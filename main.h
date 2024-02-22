47d5698a7e1b68b9ffe588ef8bd5abc7a4114fda
NaderAlAyass <132232855+NaderAlAyass@users.noreply.github.com>
2024-02-22 00:39:31 -0800

    Add files via upload

---
+++ main.h
@@ -0,0 +1,22 @@
+#define SEC_LED     PORTDbits.RD7       // Defines SEC_LED as 
+#define NS_RED      PORTAbits.RA5       // Defines NS_RED as 
+#define NS_GREEN    PORTCbits.RC0       // Defines NS_GREEN as 
+
+#define NSLT_RED    PORTDbits.RD2       // Defines NS_LT RED as 
+#define NSLT_GREEN  PORTDbits.RD3       // Defines NS_LT GREEN as 
+
+#define EW_RED      PORTDbits.RD0       // Defines EW_RED as 
+#define EW_GREEN    PORTDbits.RD1       // Defines EW_GREEN as 
+
+#define EWLT_RED    PORTEbits.RE0       // Defines EWLT_RED as 
+#define EWLT_GREEN  PORTEbits.RE1       // Defines EWLT_GREEN as 
+
+#define NS_LT_SW    PORTAbits.RA3       // Defines NS_LT as 
+//#define NS_PED_SW   PORTBbits.RB0       // Defines EW_LT as 
+
+#define EW_LT_SW    PORTAbits.RA4       
+//#define EW_PED_SW    PORTBbits.RB1    
+
+#define MODE_LED    PORTEbits.RE2       
+
+
